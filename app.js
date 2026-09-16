af4aa4ab192c36689bd6f7993dfa310cb80f6d09
Ngelekanyo <150139279+Ngelekanyo@users.noreply.github.com>
2026-09-17 01:14:13 +0200

    Update API_URL to new endpoint

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
    CONFIGURATION
    ========================================================= */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyTf7RqC7danMLSfEMTNRQIMYKiGJn5MVFYm3vYqqfrcvOz-jGJLl9UAIz1yOXLm3w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby5KKlWcQLctT8cjVVWihSrynoy-pqAai8z-GVkm8QpEdfwo7Jw0miyfA1bS4L1E4o/exec";
 
 
 /* =========================================================
