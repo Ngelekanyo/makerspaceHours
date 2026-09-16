ffbd05eaf67ee0bf9f31190b45e978e63acc031e
Ngelekanyo <150139279+Ngelekanyo@users.noreply.github.com>
2026-09-16 17:17:43 +0200

    Update API_URL to new Google Scripts endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
    ========================================================= */
 
 const API_URL =
-    "https://script.google.com/a/macros/dschool.org.za/s/AKfycbxiYdXiPM7OCQO-OGR3tVmbLmwe7U50CyEe37ux8m9j6DxLWirG1B5IgXD4rGAGNsfo-g/exec";
+    "https://script.google.com/macros/s/AKfycbxgTLioQ43o5K76XT6LKic0vhyt_7Uv4frFNV4WC6Ma_LZkMxFiVIggWuMLiSm065MP3w/exec";
 
 const ACTIVE_SHIFT_KEY = "dschool_active_shift";
 
